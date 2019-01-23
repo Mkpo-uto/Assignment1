7d955bd4a1ed74f855e24e5a1e527365657b0656
Mkpo-uto Edet <mkpoutoe@hotmail.com>
2019-01-23 22:50:50 +1100

    Basic arithmetic operators in Java

---
+++ src/com/company/assignment/Main.java
@@ -0,0 +1,29 @@
+package com.company.assignment;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //String myStatement = "This is the happiest day of my life";
+        //System.out.println(myStatement);
+
+        //System.out.println("This is the happiest day of my life");
+
+        //Basic operations
+         //Addition = +
+        //Subtraction "-"
+        //Multiplication "*"
+        //Division "/"
+
+        //int firstNum = 34, secondNum = 3; //sum = firstNum + secondNum,
+        //int diff = firstNum - secondNum;
+        //int product = firstNum * secondNum;
+        //int quotient = firstNum / secondNum;
+
+        //System.out.println("The sum is " + sum);
+        //System.out.println("The difference is " + diff);
+        //System.out.println("The product is " + product);
+        //System.out.println("The quotient is " + quotient);
+    }
+}
